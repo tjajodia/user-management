b799c8c715cf100fc6f4331295207540731c9b1b
Tushar Jajodia <tjajodia@amazon.com>
2023-08-13 01:35:34 +0530

    Created all Rest Endpoints with dummy response

--- src/main/java/com/learnings/self/handlers/UsersHandler.java
+++ src/main/java/com/learnings/self/handlers/UsersHandler.java
@@ -3,11 +3,8 @@ package com.learnings.self.handlers;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
-import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedScanList;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
-import com.amazonaws.services.dynamodbv2.model.GetItemResult;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
@@ -19,31 +16,45 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.learnings.self.constants.Constants.USER_TABLE;
-
 /**
  * Handler for requests to Lambda function.
  */
 public class UsersHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
+    final AmazonDynamoDB ddbClient = AmazonDynamoDBClientBuilder.standard().build();
+    final DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(ddbClient);
 
-    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-
-        Map<String, String> queryStringParameters = input.getQueryStringParameters();
-        final AmazonDynamoDB ddbClient = AmazonDynamoDBClientBuilder.standard().build();
-        final DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(ddbClient);
-
-        List<UserDao> allUsers = dynamoDBMapper.scan(UserDao.class, new DynamoDBScanExpression());
-
-        String result = new Gson().toJson(allUsers);
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent requestInput, final Context context) {
 
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         headers.put("X-Custom-Header", "application/json");
 
+        String httpMethod = requestInput.getHttpMethod();
+        String result = "";
+
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
+
         try {
+
+            switch (httpMethod) {
+                case "GET":
+                    result = getUser(requestInput, context);
+                    break;
+                case "POST":
+                    result = createUser(requestInput, context);
+                    break;
+                case "PUT":
+                    result = updateUser(requestInput, context);
+                    break;
+                case "DELETE":
+                    result = deleteUser(requestInput, context);
+                    break;
+                default:
+                    result = handleMissing(requestInput, context);
+                    break;
+            }
             return response
                     .withStatusCode(200)
                     .withBody(result);
@@ -53,4 +64,46 @@ public class UsersHandler implements RequestHandler<APIGatewayProxyRequestEvent,
                     .withStatusCode(500);
         }
     }
+
+    private String handleMissing(APIGatewayProxyRequestEvent requestInput, Context context) {
+        return "";
+
+    }
+
+    private String deleteUser(APIGatewayProxyRequestEvent requestInput, Context context) {
+        return "";
+
+    }
+
+    private String updateUser(APIGatewayProxyRequestEvent requestInput, Context context) {
+        return "";
+
+    }
+
+    private String createUser(APIGatewayProxyRequestEvent requestInput, Context context) {
+        return "";
+    }
+
+    private String getUser(APIGatewayProxyRequestEvent requestInput, Context context) {
+        Map<String, String> queryStringParameters = requestInput.getQueryStringParameters();
+        Map<String, String> pathParameters = requestInput.getPathParameters();
+        String pathParametersString = new Gson().toJson(pathParameters);
+        String queryStringParametersString = new Gson().toJson(queryStringParameters);
+        LambdaLogger logger = context.getLogger();
+
+        logger.log(String.format("getUser pathParams ::%s and queryParams ::%s", pathParametersString, queryStringParametersString));
+
+        if (queryStringParameters != null && queryStringParameters.containsKey("userId")) {
+            String userId = queryStringParameters.get("userId");
+            UserDao user = dynamoDBMapper.load(UserDao.class, userId);
+            return new Gson().toJson(user);
+
+        } else {
+            List<UserDao> allUsers = dynamoDBMapper.scan(UserDao.class, new DynamoDBScanExpression());
+            return new Gson().toJson(allUsers);
+        }
+
+
+    }
 }
+
